081f2d5e9048a5c445bd3611b25db741f599056c
Glenn <glenn@gcampbell.co.uk>
2021-09-01 00:15:46 +0100

    added item.service.ts

---
+++ src/item.ts
@@ -0,0 +1,13 @@
+export class Item {
+
+    readonly id: number;
+
+    readonly name: string;
+
+    readonly price: number;
+
+    readonly description: string;
+
+    readonly image: string;
+    
+}
